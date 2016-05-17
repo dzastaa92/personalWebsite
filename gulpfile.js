d703cf53e96eeff4b370b1e3228f8f871ef01f02
Justyna <justyna.wojciechowska04@gmail.com>
2016-05-17 14:02:32 +0200

    updated portfolio, image and minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var min_css = require('gulp-minify-css');
 
 gulp.task('sass', function () {
     return gulp.src('sass_files/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('pre-css'));
 });
 
 gulp.task('watch', function () {
@@ -15,4 +16,9 @@ gulp.task('watch', function () {
 gulp.task('default', function() {
     gulp.start('sass');
     gulp.start('watch');
+});
+gulp.task('minify-css', function () {
+    return gulp.src('pre-css/AllStyles.css')
+        .pipe(min_css({keepSpecialComments: 1}))
+        .pipe(gulp.dest('css'))
 });
